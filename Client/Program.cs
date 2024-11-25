3d3c4ddad0cf3b32786a87cb67d6a68a38594987
H1Kt0 <konovalchuk.denis@gmail.com>
2024-11-26 01:35:24 +0300

    Добавил в клиент обращение к серверам.
    * Добавил в клиент ссылку на TcpLibrary для получения ip и портов серверов.
    * Добавил в клиент асинхронный метод, который будет раздавать задачи
      серверам и ожидать от них ответа.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System.Net.Sockets;
 using System.Text;
 using Matrices;
-using network;
+using TcpLibrary;
 
 public partial class Program {
-  private static void Main(string[] args) {
-    TcpHelper.ReadIps("../ComputingNodes.txt");
+  private static List<Task> _tasks = new();
 
-    using (TcpClient client = new(TcpHelper.Ips[0].ip, TcpHelper.Ips[0].port)) {
-      System.Console.WriteLine("подключено к серверу");
+  private static async Task Main(string[] args) {
+    var settings = ServersHelper.ReadServers("../Servers.json");
 
+    foreach (var server in settings.Servers) {
+      using (TcpClient client = new(server.Ip, server.Port)) {
+        Console.WriteLine($"подключено к серверу {server.Ip}:{server.Port}");
+
+        _tasks.Add(PushData(client));
+      }
+    }
+
+    await Task.WhenAll(_tasks);
+    System.Console.WriteLine("all tasks completed");
+  }
+
+  private static async Task PushData(TcpClient client) {
       NetworkStream stream = client.GetStream();
 
       string message = "Hello, world!";
@@ -18,8 +30,8 @@ public partial class Program {
       stream.Write(data, 0, data.Length);
 
       System.Console.WriteLine("Данные отправлены");
-    }
   }
+
 	BlockMatrix z = new(new List<List<double>>() {
     new() {2, 2, 3, 4},
     new() {4, 5, 6, 1},
