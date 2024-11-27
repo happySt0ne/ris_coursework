65a4e4fc47394146cc6167f4e7107058da06d7a1
H1Kt0 <konovalchuk.denis@gmail.com>
2024-11-27 18:38:23 +0300

    Рефакторинг класса `Client`

--- Client/Client.cs
+++ Client/Client.cs
@@ -9,21 +9,21 @@ namespace Client;
 
 public static class Client { 
   private static List<Task<List<Matrix>>> _tasks = new();
-	private static BlockMatrix _A 
-      = new(new List<List<double>>() {
-    new() {2, 2, 3, 4},
-    new() {4, 5, 6, 1},
-    new() {2, 3, 4, 5},
-    new() {5, 2, 1, 3},
-  });
-  private static BlockMatrix _B 
-      = new(new List<List<double>>() {
-    new() {7, 8, 2, 3},
-    new() {9, 3, 4, 1},
-    new() {2, 3, 3, 4},
-    new() {5, 1, 2, 6},
-  });
+
+	private static BlockMatrix? _A;
+  private static BlockMatrix? _B;
+    
   private static int _rowNumber = 0;
+  private static string? _serializedB;
+
+  public static void SetMatrixA(List<List<double>> a) {
+    _A = new(a);
+  }
+
+  public static void SetMatrixB(List<List<double>> b) {
+    _B = new(b);
+    _serializedB = JsonConvert.SerializeObject(_B);
+  }
 
   public static async Task Start(string pathToServers) {
     var settings = ServersHelper.ReadServers(pathToServers);
@@ -41,37 +41,63 @@ public static class Client {
   private static async Task CreateTasks(ServerSettings settings) {
     foreach (var server in settings.Servers) {
       TcpClient client = new();
+
       await client.ConnectAsync(server.Ip, server.Port);
-      Console.WriteLine($"подключено к серверу {server.Ip}:{server.Port}");
+      Console.WriteLine(
+        $"подключено к серверу {server.Ip}:{server.Port}");
 
       _tasks.Add(PushData(client));
     }
   }
-  
+
   private static async Task<List<Matrix>> PushData(TcpClient client) {
     NetworkStream stream = client.GetStream();
 
+    string jsonToSend = PrepareData();
+    await SendData(jsonToSend, stream);
+    var result = await ReadResponce(stream);
+
+    client.Close();
+    return result;
+  }
+
+  private static string PrepareData() {
+    if (_A is null) 
+      throw new NullReferenceException("matrix A is null");
+    if (_B is null)
+      throw new NullReferenceException("matrix B is null");
+
     List<Matrix> list = _A.GetRow(_rowNumber++);
-
     string rowJson = JsonConvert.SerializeObject(list);
-    string matrixJson = JsonConvert.SerializeObject(_B); // вот эту хуйню можно вынести отдельно из цикла что б не повторять.
+    string jsonToSend = $"{rowJson}||{_serializedB}";
 
-    string jsonResult = $"{rowJson}||{matrixJson}";
+    return jsonToSend;
+  }
 
+  private static async Task SendData(
+      string jsonToSend, NetworkStream stream) {
     byte[] dataBytes = new byte[256];
-    dataBytes = Encoding.UTF8.GetBytes(jsonResult);
+    dataBytes = Encoding.UTF8.GetBytes(jsonToSend);
     
     System.Console.WriteLine("Данные отправлены");
     await stream.WriteAsync(dataBytes, 0, dataBytes.Length);
+  }
 
+  private static async Task<List<Matrix>> ReadResponce(
+      NetworkStream stream) {
     var responceByte = new byte[256]; 
+
     await stream.ReadAsync(responceByte, 0, responceByte.Length); 
     string responce = Encoding.UTF8
       .GetString(responceByte, 0, responceByte.Length);
-    var result = JsonConvert.DeserializeObject<List<Matrix>>(responce);
-    if (result is null) throw new NullReferenceException();
+    
+    var result = JsonConvert
+      .DeserializeObject<List<Matrix>>(responce);
+    if (result is null) {
+      throw new NullReferenceException(
+        "can't get the result object");
+    }
 
-    client.Close();
     return result;
   }
 }
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,26 @@
-﻿public partial class Program {
+﻿namespace main;
+
+using Client = Client.Client;
+
+public class Program {
   private static async Task Main(string[] args) {
-    await Client.Client.Start("../Servers.json");
+    Client.SetMatrixA(
+      new(new List<List<double>>() {
+        new() {2, 2, 3, 4},
+        new() {4, 5, 6, 1},
+        new() {2, 3, 4, 5},
+        new() {5, 2, 1, 3},
+      })
+    );
+    Client.SetMatrixB(
+      new(new List<List<double>>() {
+        new() {7, 8, 2, 3},
+        new() {9, 3, 4, 1},
+        new() {2, 3, 3, 4},
+        new() {5, 1, 2, 6},
+      })
+    );
+
+    await Client.Start("../Servers.json");
   }
 }
